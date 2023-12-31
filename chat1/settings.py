8f62f67924e2791e1489e79c4bafa29c74fa4780
avneeshdeshmukh <avneesh1745@gmail.com>
2023-12-31 11:53:33 +0530

    New Commit

--- chat1/settings.py
+++ chat1/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -125,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
